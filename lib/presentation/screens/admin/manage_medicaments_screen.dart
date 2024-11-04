ff6c2ef584313cf74de2bb32c015f40321f4cf22
damian-pebe <damian.pebe@gmail.com>
2024-11-04 10:11:42 -0600

    medicaments module finished

--- lib/presentation/screens/admin/manage_medicaments_screen.dart
+++ lib/presentation/screens/admin/manage_medicaments_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:connectcare/core/constants/constants.dart';
+import 'package:connectcare/presentation/screens/admin/update_medicament.dart';
+import 'package:connectcare/presentation/widgets/snack_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ManageMedications extends StatefulWidget {
   const ManageMedications({super.key});
@@ -8,23 +14,49 @@ class ManageMedications extends StatefulWidget {
 }
 
 class _ManageMedicationsState extends State<ManageMedications> {
-  List<String> medicaments = ['Paracetamol local', 'Ibuprofeno local'];
-  List<String> filterMedicaments = [];
+  List<Map<String, dynamic>> medicaments = [];
+  List<Map<String, dynamic>> filterMedicaments = [];
   TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    filterMedicaments = medicaments;
+    medicamentsInit();
+  }
+
+  Future<void> medicamentsInit() async {
+    var url = Uri.parse('$baseUrl/medicamento');
+    var response = await http.get(url);
+    final List<dynamic> data = json.decode(response.body);
+    setState(() {
+      medicaments = data.map((item) {
+        return {
+          'id': item['id_medicamento'].toString(),
+          'nombre': item['nombre'],
+        };
+      }).toList();
+      filterMedicaments = List.from(medicaments);
+    });
   }
 
   void updateFilter(String query) {
     setState(() {
       filterMedicaments = medicaments
           .where((medicament) =>
-              medicament.toLowerCase().contains(query.toLowerCase()))
+              medicament['nombre'].toLowerCase().contains(query.toLowerCase()))
           .toList();
     });
+  }
+
+  Future<void> deleteMedicament(String id) async {
+    var url = Uri.parse('$baseUrl/medicamento/$id');
+    var response = await http.delete(url);
+    if (mounted) {
+      responseHandlerDelete(response, context, 'Medicamento creado con exito',
+          'Error al crear medicamento');
+    }
+    medicamentsInit();
+    setState(() {});
   }
 
   @override
@@ -63,10 +95,13 @@ class _ManageMedicationsState extends State<ManageMedications> {
               ),
               const SizedBox(height: 20),
               filterMedicaments.isEmpty
-                  ? const Center(
-                      child: Text(
-                        'Nada que ver por aquí',
-                        style: TextStyle(fontSize: 18),
+                  ? SizedBox(
+                      height: 450,
+                      child: const Center(
+                        child: Text(
+                          'Nada que ver por aquí',
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ),
                     )
                   : SizedBox(
@@ -81,19 +116,35 @@ class _ManageMedicationsState extends State<ManageMedications> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 Text(
-                                  item,
+                                  item['nombre'],
                                   style: theme.textTheme.headlineLarge,
                                 ),
                                 Row(
                                   children: [
                                     IconButton(
                                       icon: Icon(Icons.edit),
-                                      onPressed: () {},
+                                      onPressed: () async {
+                                        final result = await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                UpdateMedicamentScreen(
+                                                    id: item['id']),
+                                          ),
+                                        );
+
+                                        if (result == 'updated') {
+                                          await medicamentsInit();
+                                        }
+                                      },
                                     ),
-                                    SizedBox(width: 8),
+                                    const SizedBox(width: 8),
                                     IconButton(
                                       icon: Icon(Icons.delete),
-                                      onPressed: () {},
+                                      onPressed: () async {
+                                        await deleteMedicament(item['id']);
+                                        setState(() {});
+                                      },
                                     ),
                                   ],
                                 ),
@@ -105,8 +156,12 @@ class _ManageMedicationsState extends State<ManageMedications> {
                     ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/createMedicamentScreen');
+                onPressed: () async {
+                  final result = await Navigator.pushNamed(
+                      context, '/createMedicamentScreen');
+                  if (result == 'created') {
+                    await medicamentsInit();
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                   padding:
