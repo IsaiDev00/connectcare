b1fb9df56fa739550f8beb2eeccfa03251c5d73c
damian-pebe <damian.pebe@gmail.com>
2024-11-13 14:33:33 -0600

    removed empty const

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,2 @@
 const String baseUrl =
     'https://connectcare-queries-158294687720.us-central1.run.app';
-
-//I DONT THINK I SHOULD PUT THIS HERE, THIS IS NOT DEPLOYMENT SO...
-const resendApiKey = '';
